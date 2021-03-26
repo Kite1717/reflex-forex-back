56f7b0f75eb8dd0363e8d1df838bdc75e323f139
Kite1717 <mustafafirat.yilmaz@ogr.deu.edu.tr>
2021-03-27 01:45:43 +0300

    make it better

--- app/server.js
+++ app/server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 const { sequelize } = require('./models/index');
-
+var cors = require('cors')
 // Settings
 const PORT = process.env.PORT || 4000;
-
+app.use(cors())
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
