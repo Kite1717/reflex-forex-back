9fd128f7e7c08bf63e4dd526cc9bc5e0660fc438
Kite1717 <mustafafirat.yilmaz@ogr.deu.edu.tr>
2021-04-17 18:50:24 +0300

    init files

--- app/services/transactions.js
+++ app/services/transactions.js
@@ -24,131 +24,151 @@ const app = express.Router();
 
 //   bu isteği sonra logla başka bir table !!!
 app.post("/account-deposit", async (req, res) => {
-  forex.accountDeposit(req.body).then((fores) => {
-    if (fores) {
-      forex.getUser({ Login: req.body.Login }).then((account) => {
-        if (account.Login) {
-          let result = account;
-          result.updatedAt = new Date();
+  forex
+    .accountDeposit(req.body)
+    .then((fores) => {
+      if (fores) {
+        forex.getUser({ Login: req.body.Login }).then((account) => {
+          if (account.Login) {
+            let result = account;
+            result.updatedAt = new Date();
 
-          db.Account.update(result, { where: { Login: req.body.Login } })
-            .then((acc) => {
-              return res.json({
-                data: acc,
-                status: 1,
+            db.Account.update(result, { where: { Login: req.body.Login } })
+              .then((acc) => {
+                return res.json({
+                  data: acc,
+                  status: 1,
+                });
+              })
+              .catch((err) => {
+                return res.status(500).json({ msg: "DB error", status: 0 });
               });
-            })
-            .catch((err) => {
-              return res.status(500).json({ msg: "DB error", status: 0 });
-            });
-        } else {
-          return res
-            .status(404)
-            .json({ msg: "Forex Error User Not Found", status: 0 });
-        }
-      });
-    } else {
-      return res
-        .status(404)
-        .json({ msg: "Forex Error Deposit Error", status: 0 });
-    }
-  });
+          } else {
+            return res
+              .status(404)
+              .json({ msg: "Forex Error User Not Found", status: 0 });
+          }
+        });
+      } else {
+        return res
+          .status(404)
+          .json({ msg: "Forex Error Deposit Error", status: 0 });
+      }
+    })
+    .catch(() => {
+      return res.status(500).json({ msg: "Forex error", status: 0 });
+    });
 }); // end of account deposit
 
 app.post("/account-withdraw", async (req, res) => {
-  forex.accountWithdraw(req.body).then((fores) => {
-    if (fores) {
-      forex.getUser({ Login: req.body.Login }).then((account) => {
-        if (account.Login) {
-          let result = account;
-          result.updatedAt = new Date();
+  forex
+    .accountWithdraw(req.body)
+    .then((fores) => {
+      if (fores) {
+        forex.getUser({ Login: req.body.Login }).then((account) => {
+          if (account.Login) {
+            let result = account;
+            result.updatedAt = new Date();
 
-          db.Account.update(result, { where: { Login: req.body.Login } })
-            .then((acc) => {
-              return res.json({
-                data: acc,
-                status: 1,
+            db.Account.update(result, { where: { Login: req.body.Login } })
+              .then((acc) => {
+                return res.json({
+                  data: acc,
+                  status: 1,
+                });
+              })
+              .catch((err) => {
+                return res.status(500).json({ msg: "DB error", status: 0 });
               });
-            })
-            .catch((err) => {
-              return res.status(500).json({ msg: "DB error", status: 0 });
-            });
-        } else {
-          return res
-            .status(404)
-            .json({ msg: "Forex Error User Not Found", status: 0 });
-        }
-      });
-    } else {
-      return res
-        .status(404)
-        .json({ msg: "Forex Error WithDraw Error", status: 0 });
-    }
-  });
+          } else {
+            return res
+              .status(404)
+              .json({ msg: "Forex Error User Not Found", status: 0 });
+          }
+        });
+      } else {
+        return res
+          .status(404)
+          .json({ msg: "Forex Error WithDraw Error", status: 0 });
+      }
+    })
+    .catch(() => {
+      return res.status(500).json({ msg: "Forex error", status: 0 });
+    });
 }); // end of account withdraw
 
 app.post("/credit-in", async (req, res) => {
-  forex.creditIn(req.body).then((fores) => {
-    if (fores) {
-      forex.getUser({ Login: req.body.Login }).then((account) => {
-        if (account.Login) {
-          let result = account;
-          result.updatedAt = new Date();
+  forex
+    .creditIn(req.body)
+    .then((fores) => {
+      if (fores) {
+        forex.getUser({ Login: req.body.Login }).then((account) => {
+          if (account.Login) {
+            let result = account;
+            result.updatedAt = new Date();
 
-          db.Account.update(result, { where: { Login: req.body.Login } })
-            .then((acc) => {
-              return res.json({
-                data: acc,
-                status: 1,
+            db.Account.update(result, { where: { Login: req.body.Login } })
+              .then((acc) => {
+                return res.json({
+                  data: acc,
+                  status: 1,
+                });
+              })
+              .catch((err) => {
+                return res.status(500).json({ msg: "DB error", status: 0 });
               });
-            })
-            .catch((err) => {
-              return res.status(500).json({ msg: "DB error", status: 0 });
-            });
-        } else {
-          return res
-            .status(404)
-            .json({ msg: "Forex Error User Not Found", status: 0 });
-        }
-      });
-    } else {
-      return res
-        .status(404)
-        .json({ msg: "Forex Error Credit In Error", status: 0 });
-    }
-  });
+          } else {
+            return res
+              .status(404)
+              .json({ msg: "Forex Error User Not Found", status: 0 });
+          }
+        });
+      } else {
+        return res
+          .status(404)
+          .json({ msg: "Forex Error Credit In Error", status: 0 });
+      }
+    })
+    .catch(() => {
+      return res.status(500).json({ msg: "Forex error", status: 0 });
+    });
 }); // end of account credit in
 
 app.post("/credit-out", async (req, res) => {
-  forex.creditOut(req.body).then((fores) => {
-    if (fores) {
-      forex.getUser({ Login: req.body.Login }).then((account) => {
-        if (account.Login) {
-          let result = account;
-          result.updatedAt = new Date();
+  forex
+    .creditOut(req.body)
+    .then((fores) => {
+      if (fores) {
+        forex.getUser({ Login: req.body.Login }).then((account) => {
+          if (account.Login) {
+            let result = account;
+            result.updatedAt = new Date();
 
-          db.Account.update(result, { where: { Login: req.body.Login } })
-            .then((acc) => {
-              return res.json({
-                data: acc,
-                status: 1,
+            db.Account.update(result, { where: { Login: req.body.Login } })
+              .then((acc) => {
+                return res.json({
+                  data: acc,
+                  status: 1,
+                });
+              })
+              .catch((err) => {
+                return res.status(500).json({ msg: "DB error", status: 0 });
               });
-            })
-            .catch((err) => {
-              return res.status(500).json({ msg: "DB error", status: 0 });
-            });
-        } else {
-          return res
-            .status(404)
-            .json({ msg: "Forex Error User Not Found", status: 0 });
-        }
-      });
-    } else {
-      return res
-        .status(404)
-        .json({ msg: "Forex Error Credit Out Error", status: 0 });
-    }
-  });
+          } else {
+            return res
+              .status(404)
+              .json({ msg: "Forex Error User Not Found", status: 0 });
+          }
+        });
+      } else {
+        return res
+          .status(404)
+          .json({ msg: "Forex Error Credit Out Error", status: 0 });
+      }
+    })
+    .catch(() => {
+      return res.status(500).json({ msg: "Forex error", status: 0 });
+    });
 }); // end of account credit out
 
 module.exports = app;
